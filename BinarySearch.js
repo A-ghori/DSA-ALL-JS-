784db599799b313aadc2a64b06e7d129c32c0f77
Shubhayu Barua <youremail@example.com>
2025-07-31 22:18:07 +0530

    New Files

--- BinarySearch.js
+++ BinarySearch.js
@@ -3,7 +3,8 @@ var binarySearch =  function (arr,target){
     let end = arr.length - 1 ;
 
     while(start <= end){
-        let mid = Math.floor((start + end) / 2);
+        let mid = start + Math.floor((end - start) / 2); //For control the intmax overflow for big int numbers 
+        // let mid = Math.floor((start + end)/2)
         if(target > arr[mid]){
             start = mid + 1;
         }
@@ -19,7 +20,7 @@ var binarySearch =  function (arr,target){
     return -1;
 }
 
-const arr = [1, 3, 5, 7, 9, 11];
+const arr = [1, 3, 5, 7, 9];
 const target = 7;
 
 console.log("Target index:", binarySearch(arr, target));
