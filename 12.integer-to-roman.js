c48437c6abf918518bdcbcc858e3adafdff97f95
Shubhayu <shubhayu123@gmail.com>
2025-09-21 03:20:18 +0530

    NEW

--- 12.integer-to-roman.js
+++ 12.integer-to-roman.js
@@ -10,7 +10,19 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    
+    let result = "";
+    let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+let symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+
+
+for(let i=0; i<values.length; i++){
+while(num >= values[i]){
+result = result + symbols[i]
+num = num - values[i]
+}
+
+}
+return result
 };
 // @lc code=end
 
