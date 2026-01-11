dec2aa2d9ca36242dd6dfd7344343219164e7ba8
A-ghori <shubhayubarua244@gmail.com>
2026-01-11 08:58:50 +0000

    Fresh Fuck

--- RotateArray.js
+++ RotateArray.js
@@ -6,6 +6,18 @@
 let arr = [1,2,3,4,5];
 let d = 2;
 let n = arr.length;
-let normalised = ((d%n)+n)%n;
-arr.push(...arr.splice(0,normalised));
-console.log("Array is", arr);
+//let normalised = ((d%n)+n)%n;
+//arr.push(...arr.splice(0,normalised));
+//console.log("Array is", arr);
+
+let arr1 = [];
+// First d to end array element revese;
+for(let i=arr.length - 1; i>=d; i--){
+arr1.push(arr[i]);
+}
+// Next d to 0 reverse;
+for(let i = d - 1;  i >= 0; i--){
+arr1.push(arr[i])
+}
+
+console.log(arr1)
